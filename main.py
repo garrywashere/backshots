0a65e5a6a16ac9127ab7aae2481fdcb29c498b89
Garry <github@garrynet.co.uk>
2025-01-01 14:14:19 +0200

    Prototype functionality...
     - Added ability to specify rounds
     - Need to actually give the ability to subtract rounds (shots) during rounds
     - Need to change namings, because rounds and rounds are confusing
     - Need to switch to client-side JavaScript for handling calculations and data

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+round_info = {"lives": 1, "blanks": 2, "probability": "50%"}
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/new_round")
+def new_round():
+    return render_template("new_round.html")
+
+
+@app.route("/current_round", methods=["GET", "POST"])
+def current_round():
+    if request.method == "POST":
+        round_info["lives"] = int(request.form["live"])
+        round_info["blanks"] = int(request.form["blank"])
+
+        total = round_info["lives"] + round_info["blanks"]
+        round_info["probability"] = f"{round(round_info['lives']/total*100,2)}%"
+
+    return render_template("current_round.html", round_info=round_info)
 
 
 if __name__ == "__main__":
